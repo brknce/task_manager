2fbf8caf743fc3526ff9032fdc4daace5d577b81
brknce <brknce19@gmail.com>
2020-10-09 19:22:54 +0300

    TabNavigator was added

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import Main from './Components/Main';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { FontAwesome5 } from "@expo/vector-icons";
+import {HomeScreen, AddTaskScreen, ProfileScreen} from "./Screens/index"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Main />
-    );
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="home" size={28} color="#CDCCCE" />
+      }
+    },
+    AddTask: {
+      screen: AddTaskScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="plus" size={28} color="#CDCCCE" />
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="user" size={28} color="#CDCCCE" />
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: '#81B247',
+    }
   }
-}
+);
+
+export default createAppContainer(TabNavigator);
---
+++ Screens/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AddTask from "./AddTask" 
+import Home from "./Home"
+import Profile from "./Profile" 
+
+export const HomeScreen = () => <Home/>
+export const AddTaskScreen = () => <AddTask/>
+export const ProfileScreen = () => <Profile/>
